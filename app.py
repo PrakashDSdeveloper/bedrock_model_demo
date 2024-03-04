fc9e17fc0a3a13d082481411b1357bb439b8e758
Prakash T N <prakashph79@gmail.com>
2024-03-04 20:34:09 +0530

    model_test

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify, render_template
+from test import demo_memory, demo_conversation
+
+app = Flask(__name__)
+
+memory = demo_memory()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    input_text = request.json.get('input_text')
+    chat_response = demo_conversation(input_text=input_text, memory=memory)
+    return jsonify({'response': chat_response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/scripts.js
@@ -0,0 +1,23 @@
+// // responsive.js
+
+// // Function to handle responsiveness
+// function handleResponsive() {
+//     // Get the width of the viewport
+//     var screenWidth = window.innerWidth;
+
+//     // Example: Adjust the font size of a heading based on screen width
+//     var heading = document.querySelector('.h4-headers');
+//     if (screenWidth < 768) {
+//         heading.style.fontSize = '16px'; // Adjust font size for smaller screens
+//     } else {
+//         heading.style.fontSize = '24px'; // Default font size for larger screens
+//     }
+
+//     // Add more responsive adjustments here as needed
+// }
+
+// // Call the function initially
+// handleResponsive();
+
+// // Listen for window resize events and call the function again
+// window.addEventListener('resize', handleResponsive);
